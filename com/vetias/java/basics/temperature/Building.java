c949d6b73fbab1acb4f6c529f3810af63139e2e0
dharaneesh20834 <dharaneeshs24csa@vetias.ac.in>
2025-07-04 14:51:45 +0530

    building

---
+++ com/vetias/java/basics/temperature/Building.java
@@ -0,0 +1,11 @@
+
+import java.time.LocalDateTime;
+public class Building {
+    private String name;
+    private String area;
+    private double floors;
+    private LocalDateTime opendatetime;
+    private LocalDateTime closedatetime;
+
+
+}
