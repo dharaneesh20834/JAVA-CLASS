fadd1d6c8b35d80334481d31ce08640a98be0a30
dharaneesh20834 <dharaneeshs24csa@vetias.ac.in>
2025-07-04 14:54:23 +0530

    floor

---
+++ com/vetias/java/basics/temperature/Floor.java
@@ -0,0 +1,26 @@
+
+public class Floor{
+    private int floorName;
+    private double number;
+
+    public Floor(int floorName, double number) {
+        this.floorName = floorName;
+        this.number = number;
+    }
+
+    public int getFloorName() {
+        return floorName;
+    }
+
+    public double getNumber() {
+        return number;
+    }
+
+    public void setFloorName(int floorName) {
+        this.floorName = floorName;
+    }
+
+    public void setNumber(double number) {
+        this.number = number;
+    }
+}
