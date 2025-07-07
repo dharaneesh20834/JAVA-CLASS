cf3142fe103b63eb24f326f2fc5aee6f639b26e6
dharaneesh20834 <dharaneeshs24csa@vetias.ac.in>
2025-07-07 10:46:12 +0530

    string

---
+++ com/vetias/java/basics/temperature/stringa.java
@@ -0,0 +1,13 @@
+public class string {
+    public static void main(String[] args) {
+       // String firstName="Dharaneesh";
+       //String lastName="s";
+       // String finalName=firstName.concat(lastName);
+       // System.out.println(finalName.toupperCase());
+       String sentance = "This proogram shows how we can we spplit a string into multiple strings";
+       String words []=sentance.split(regex:"");
+       System.out.println(words.length);
+    }
+
+
+}
