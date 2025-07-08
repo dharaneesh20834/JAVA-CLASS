e1d74de09a56955090b244cf31792c2cc2048035
dharaneesh20834 <dharaneeshs24csa@vetias.ac.in>
2025-07-08 15:26:27 +0530

    calx

---
+++ com/vetias/java/basics/Calculator.java
@@ -0,0 +1,23 @@
+
+
+public class Calculator {
+   
+        public int division(int a,int b){
+
+         return a/b;
+        }
+    
+    public int addition(int a,int b){
+    
+    return a/b;
+    }
+    public int subtract(int a,int b){
+    
+    return a/b;
+    } public static void main(String[] args) {
+        Calculator calculator =new Calculator();
+        System.out.println("division:"+calculator.division(6, 0));
+        System.out.println("Addition:"+calculator.addition(1,02));
+        System.out.println("subtraction :"+calculator.subtract(8,5));
+    }
+}
